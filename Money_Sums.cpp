652ccf2f8c3c5c5511f18adb7d4d22adde5f636b
Zhen Su <blair1306@163.com>
2025-07-02 00:25:37 +0800

    When doing subset sum DP (each coin can be used once), you must iterate backwards over j

--- Money_Sums.cpp
+++ Money_Sums.cpp
@@ -63,7 +63,7 @@ int main()
     */
     dp[0] = 1;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = N - 1; j >= 0; j--) {
             if (dp[j] != -1) {
                 dp[j + coin[i]] = 1;
             }
