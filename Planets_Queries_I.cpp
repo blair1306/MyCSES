58e7367af9a28ff0a39b835e99291007667e9483
Zhen Su <blair1306@163.com>
2025-07-06 22:22:45 +0800

    but why doens't k&-k work?

---
+++ Planets_Queries_I.cpp
@@ -0,0 +1,83 @@
+#if defined(__linux__)
+// Linux code
+#include <bits/stdc++.h>
+#else
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 12;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+vvi pre(33, vi(N, -1));
+
+int travel(int x, int k)
+{
+    int n;
+    int dest = x;
+    for (int b = 0; b < 32; b++) {
+        if (k & 1 << b) {
+            x = pre[b][x];
+        }
+    }
+    return x;
+}
+
+int main()
+{
+    // Fast I/O
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n, q;
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> pre[0][i];
+    }
+
+    for (int i = 1; i <= 32; i++) {
+        for (int j = 1; j <= n; j++) {
+            pre[i][j] = pre[i - 1][pre[i - 1][j]];
+        }
+    }
+
+    dbg(pre);
+    for (int i = 0; i < q; i++) {
+        int x, k;
+        cin >> x >> k;
+        cout << travel(x, k) << endl;
+    }
+
+    return 0;
+}
