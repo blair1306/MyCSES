2c759da7850962bd5dd1fe1c7c032e3144696986
Zhen Su <blair1306@163.com>
2025-07-06 16:52:18 +0800

    only push to pq when new distance of node is strictly smaller

---
+++ Investigation.cpp
@@ -0,0 +1,113 @@
+#if defined(__linux__)
+// Linux code
+#include <bits/stdc++.h>
+#else
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 1e5 + 1;  // size for global arrays (if needed)
+#endif
+
+using wE = pair<ll, int>;
+vector<wE> wG[N];
+vll d(N, LINF);
+vi pre[N];
+int n;
+vll dp(N, 0);
+vi dp1(N, INF);
+vi dp2(N, 0);
+
+int main()
+{
+    // Fast I/O
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        ll c;
+        cin >> a >> b >> c;
+        wG[a].push_back({c, b});
+    }
+
+    // dijkstra with modification
+    priority_queue<wE, vector<wE>, greater<wE>> q;
+    q.push({0, 1});
+    d[1] = 0;
+    while (q.size()) {
+        auto [tmp, u] = q.top();
+        q.pop();
+
+        if (d[u] < tmp) continue;
+
+        for (auto [w, v] : wG[u]) {
+            int nw = d[u] + w;
+            if (nw <= d[v]) {
+                if (nw < d[v]) {
+                    pre[v].clear();
+                    q.push({nw, v});
+                }
+                d[v] = nw;
+                pre[v].push_back(u);
+            }
+        }
+    }
+
+    dp[1] = 1;
+    dp1[1] = 0;
+    dp2[1] = 0;
+    for (int u = 1; u <= n; u++) {
+        for (auto v : pre[u]) {
+            dp[u] = (dp[u] + dp[v]) % MOD;
+            dp1[u] = min(dp1[u], dp1[v] + 1);
+            dp2[u] = max(dp2[u], dp2[v] + 1);
+        }
+    }
+
+    dbg(d);
+    dbg(pre);
+    dbg(dp);
+    dbg(dp1);
+    dbg(dp2);
+
+    cout << d[n] << ' ';
+    cout << dp[n] << ' ';
+    cout << dp1[n] << ' ';
+    cout << dp2[n] << ' ';
+
+    return 0;
+}
