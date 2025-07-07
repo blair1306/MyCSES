8fd9231c1dcc3bf3d02951c0b43bdb1a9f7fa1c6
Zhen Su <blair1306@163.com>
2025-07-07 23:14:46 +0800

    TLE with my best effort optimization(which is little)

---
+++ Word_Combinations.cpp
@@ -0,0 +1,88 @@
+#if defined(__linux__)
+// Linux code
+#include <bits/stdc++.h>
+#else
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+int visited[N];
+
+int main()
+{
+    // Fast I/O
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    string p;
+    cin >> p;
+    int k;
+
+    unordered_set<string> dict;
+    cin >> k;
+    int max_len = -1;
+    for (int i = 0; i < k; i++) {
+        string word;
+        cin >> word;
+        max_len = max(max_len, (int)word.length());
+        dict.insert(word);
+        if (word.length() <= p.length()) visited[word.length()] = 1;
+    }
+
+    vi dp(p.size() + 1, 0);
+    dp[0] = 1;
+
+    for (int i = 1; i <= p.size(); i++) {
+        for (int len = 1; len <= i; len++) {
+            int pos = i - len;
+            if (dp[pos] == 0) continue;
+            if (!visited[len]) continue;
+            if (len > max_len) break;
+            string sub = p.substr(pos, len);
+            dbg(sub);
+            if (dict.count(sub)) {
+                dp[i] = (dp[i] + dp[pos]) % MOD;
+            }
+        }
+    }
+
+    dbg(dp);
+
+    cout << dp[p.size()] << endl;
+
+    return 0;
+}
