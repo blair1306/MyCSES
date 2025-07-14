66deca3ee6d08e5577028d0a620bc4df33522e52
Zhen Su <blair1306@163.com>
2025-07-14 23:02:16 +0800

    Kosaraju's wrong implementation

---
+++ Flight_Routes_Check.cpp
@@ -0,0 +1,126 @@
+#if defined(__linux__)
+// Linux code
+#include <bits/stdc++.h>
+#else
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+int comp_id = 0;
+int comp[N];
+stack<int> s;
+vi G[N];
+vi rG[N];
+vi vis(N);
+
+void dfs(int u)
+{
+    if (vis[u]) return;
+    vis[u] = 1;
+
+    for (auto v : G[u]) {
+        dfs(v);
+    }
+
+    s.push(u);
+}
+
+void dfs2(int u)
+{
+    if (vis[u]) return;
+    vis[u] = 1;
+    comp[u] = comp_id;
+
+    for (auto v : rG[u]) {
+        dfs(v);
+    }
+}
+
+int main()
+{
+    // Fast I/O
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        rG[b].push_back(a);
+    }
+
+    // Kosaraju's
+    for (int i = 1; i <= n; i++) {
+        if (vis[i]) continue;
+        dfs(i);
+    }
+
+    s.push(5);
+
+    dbg(s);
+
+    fill(all(vis), 0);
+    while (s.size()) {
+        int u = s.top();
+        s.pop();
+        if (vis[u]) continue;
+        comp_id++;
+        dfs2(u);
+    }
+
+    dbg(comp);
+
+    int u = 1, v = -1;
+    for (int i = 2; i <= n; i++) {
+        if (comp[u] != comp[i]) {
+            v = i;
+            break;
+        }
+    }
+
+    if (n > 1 && v != -1) {
+        cout << "NO" << endl;
+        cout << u << ' ' << v << endl;
+    } else {
+        cout << "YES" << endl;
+    }
+
+    return 0;
+}
