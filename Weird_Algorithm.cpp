d5222fa4cb48f2947283c7b25bda9311f2aef5da
Zhen Su <blair1306@163.com>
2025-08-13 20:32:43 +0800

    the notorious Collatz conjecture...

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,61 @@
+#if defined(__linux__)
+// Linux code
+#include <bits/stdc++.h>
+#else
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+int main()
+{
+    // Fast I/O
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    while (n >= 1) {
+        cout << n << " ";
+        if (n == 1) break;
+        if ((n & 1) == 0)
+            n /= 2;
+        else
+            n = 3 * n + 1;
+    }
+
+    return 0;
+}
