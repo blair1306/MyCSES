f94ecee80bf935ffd922c51b61ade09a93d05991
Zhen Su <blair1306@163.com>
2025-10-20 22:27:15 +0800

    You're overflowing the 32-bit int when computing n*(n+1). Cast to 64-bit before multiplication.

---
+++ Missing_Number.cpp
@@ -0,0 +1,63 @@
+#if defined(__linux__)
+// Linux code
+#include <bits/stdc++.h>
+#else
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+int main()
+{
+    // Fast I/O
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    ull ans = 0;
+    int n;
+    cin >> n;
+    ans = n * (n + 1) / 2;
+
+    for (int i = 1; i < n; i++) {
+        int num;
+        cin >> num;
+        ans -= num;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
