c8fed664f97ac3e9c4d5c38b0cb8b2f5cc14af3b
Zhen Su <blair1306@163.com>
2026-01-21 14:15:45 +0000

    test

--- Counting_Towers.cpp
+++ Counting_Towers.cpp
@@ -15,51 +15,49 @@ ll dp[N + 1][2];
 
 int main()
 {
-    // Fast I/O
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+  // Fast I/O
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
 
-    int t;
-    cin >> t;
-    int n;
+  int t;
+  cin >> t;
+  int n;
 
-    // pre-compute the dp matrix
-    /*
-        dp[x][0] number of ways when at x level we have two tiles of width 1
-        dp[x][1] ...... width 2
+  // pre-compute the dp matrix
+  /*
+      dp[x][0] number of ways when at x level we have two tiles of width 1
+      dp[x][1] ...... width 2
 
-        |_| |_|
-        |_| |_|
+      |_| |_|
+      |_| |_|
 
-        | | | |
-        |_| |_|
+      | | | |
+      |_| |_|
 
-        |_| | |
-        |_| |_|
+      |_| | |
+      |_| |_|
 
-        | | |_|
-        |_| |_|
-    */
-    dp[1][0] = 1;
-    dp[1][1] = 1;
+      | | |_|
+      |_| |_|
+  */
+  dp[1][0] = 1;
+  dp[1][1] = 1;
 
-    for (int i = 2; i <= N; i++)
-    {
-        dp[i][0] = (4 * dp[i - 1][0]) % M + dp[i - 1][1];
-        dp[i][1] = dp[i - 1][0] + (2 * dp[i - 1][1]) % M;
-        dp[i][0] %= M;
-        dp[i][1] %= M;
-    }
+  for (int i = 2; i <= N; i++) {
+    dp[i][0] = (4 * dp[i - 1][0]) % M + dp[i - 1][1];
+    dp[i][1] = dp[i - 1][0] + (2 * dp[i - 1][1]) % M;
+    dp[i][0] %= M;
+    dp[i][1] %= M;
+  }
 
-    int ans = 0;
+  int ans = 0;
 
-    for (int i = 0; i < t; i++)
-    {
-        cin >> n;
-        ans = dp[n][0] + dp[n][1];
-        ans %= M;
-        cout << ans << endl;
-    }
+  for (int i = 0; i < t; i++) {
+    cin >> n;
+    ans = dp[n][0] + dp[n][1];
+    ans %= M;
+    cout << ans << endl;
+  }
 
-    return 0;
+  return 0;
 }
