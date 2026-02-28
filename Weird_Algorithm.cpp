c07ca0218d0bc61811decef307c2f41f404d809e
Zhen Su <blair1306@163.com>
2026-02-28 20:03:28 +0800

    resubmit for testing my new local enviroment

--- Weird_Algorithm.cpp
+++ Weird_Algorithm.cpp
@@ -1,11 +1,79 @@
-#if defined(__linux__)
-// Linux code
+#if defined(__has_include)
+#if __has_include(<bits/stdc++.h>)
 #include <bits/stdc++.h>
 #else
-#include <vector>
-#include <iostream>
-#include <climits>
 #include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#endif
+#else
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
 #endif
 
 using namespace std;
@@ -22,7 +90,6 @@ typedef vector<ll> vll;
 #define endl '\n'
 #define all(x) (x).begin(), (x).end()
 
-// #define LOCAL
 #ifdef LOCAL
 #include "mydebug.h"
 #else
@@ -40,22 +107,29 @@ const int N = 10;  // size for global arrays (if needed)
 const int N = 2e5 + 5;  // size for global arrays (if needed)
 #endif
 
+void solve(ll n)
+{
+  while (n != 1) {
+    cout << n << ' ';
+    if (n & 1) {
+      n = 3 * n + 1;
+    } else {
+      n /= 2;
+    }
+  }
+  cout << 1;
+}
+
 int main()
 {
-    // Fast I/O
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+  // Fast I/O
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
 
-    ll n;
-    cin >> n;
-    while (n >= 1) {
-        cout << n << " ";
-        if (n == 1) break;
-        if ((n & 1) == 0)
-            n /= 2;
-        else
-            n = 3 * n + 1;
-    }
+  ll n;
+  cin >> n;
 
-    return 0;
+  solve(n);
+
+  return 0;
 }
