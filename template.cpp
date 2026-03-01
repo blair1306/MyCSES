59d490caaec20a51ce61f94a60ad429c7ae83065
Zhen Su <blair1306@163.com>
2026-03-01 17:53:47 +0800

    update template to use __has_include

--- template.cpp
+++ template.cpp
@@ -1,43 +1,5 @@
-#if defined(__has_include)
-#if __has_include(<bits/stdc++.h>)
+#if defined(__has_include) && __has_include(<bits/stdc++.h>)
 #include <bits/stdc++.h>
-#else
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <climits>
-#include <cmath>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <type_traits>
-#include <utility>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <deque>
-#endif
 #else
 #include <algorithm>
 #include <array>
@@ -89,6 +51,7 @@ typedef vector<ll> vll;
 // Shortcuts
 #define endl '\n'
 #define all(x) (x).begin(), (x).end()
+#define sz(x) (x).size()
 
 // #define LOCAL
 #ifdef LOCAL
