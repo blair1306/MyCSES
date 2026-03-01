2a5097db9fd7cbe0aa918b1f45c8fb6bac1d581e
Zhen Su <blair1306@163.com>
2026-03-01 17:58:29 +0800

    dp[l, r] is always for first player in a sub game

---
+++ Removal_Game.cpp
@@ -0,0 +1,105 @@
+#if defined(__has_include) && __has_include(<bits/stdc++.h>)
+#include <bits/stdc++.h>
+#else
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (x).size()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+int main()
+{
+  // Fast I/O
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n;
+  cin >> n;
+  vi number(n);
+  ll score_sum = 0;
+  vector<vll> dp(n, vll(n));  // dp[l, r] best possible score1 - score2
+  for (int i = 0; i < n; i++) {
+    int num;
+    cin >> num;
+    score_sum += num;
+    number[i] = num;
+    dp[i][i] = num;
+  }
+
+  for (int j = 1; j < n; j++) {
+    for (int i = j - 1; i >= 0; i--) {
+      dp[i][j] = max(-dp[i][j - 1] + number[j], -dp[i + 1][j] + number[i]);
+    }
+  }
+
+  dbg(dp);
+
+  ll score1 = (score_sum + dp[0][n - 1]) / 2;
+  cout << score1 << endl;
+
+  return 0;
+}
