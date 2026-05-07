9fd4c5d0c841704d5313eb669e948fc6512a453d
Zhen Su <blair1306@163.com>
2026-05-07 22:32:40 +0800

    small to large merging

---
+++ Distinct_Colors.cpp
@@ -0,0 +1,121 @@
+#if defined(__has_include) && __has_include(<bits/stdc++.h>)
+#include <bits/stdc++.h>
+#else
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (x).size()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+vi adj[N];
+unordered_set<int> colors[N];
+vi distinct_color(N);
+
+void dfs(int u, int p)
+{
+  for (int v : adj[u]) {
+    if (v == p) continue;
+    dfs(v, u);
+    if (colors[v].size() > colors[u].size()) {
+      swap(colors[v], colors[u]);
+    }
+    for (int c : colors[v]) colors[u].insert(c);
+  }
+
+  distinct_color[u] = colors[u].size();
+}
+
+int main()
+{
+  // Fast I/O
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    int c;
+    cin >> c;
+    colors[i].insert(c);
+  }
+
+  for (int i = 1; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  dfs(1, 0);
+
+  for (int i = 1; i <= n; i++) {
+    cout << distinct_color[i] << " ";
+  }
+
+  return 0;
+}
