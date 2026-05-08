7b6eb038fd7826774daee6e46e9c8fdf89d2cd4c
Zhen Su <blair1306@163.com>
2026-05-08 18:56:30 +0800

    TLE for some cases

---
+++ Finding_a_Centroid.cpp
@@ -0,0 +1,120 @@
+#if defined(__has_include) && __has_include(<bits/stdc++.h>)
+#include <bits/stdc++.h>
+#else
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#endif
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+
+// Shortcuts
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (x).size()
+
+// #define LOCAL
+#ifdef LOCAL
+#include "mydebug.h"
+#else
+#define dbg(...)
+#define dbg_arr(...)
+#endif
+
+// Constants
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;  // or 998244353
+#ifdef LOCAL
+const int N = 10;  // size for global arrays (if needed)
+#else
+const int N = 2e5 + 5;  // size for global arrays (if needed)
+#endif
+
+vi adj[N];
+
+int getSubTreeSize(int u, int p)
+{
+  int treeSize = 1;
+  for (int v : adj[u]) {
+    if (v == p) continue;
+    treeSize += getSubTreeSize(v, u);
+  }
+
+  return treeSize;
+}
+
+int n;
+int centroid = 1;
+
+void findCentroid(int u, int p)
+{
+  for (int v : adj[u]) {
+    if (v == p) continue;
+    if (getSubTreeSize(v, u) * 2 > n) {
+      return findCentroid(v, u);
+    }
+  }
+  centroid = u;
+  dbg(centroid);
+}
+
+int main()
+{
+  // Fast I/O
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  cin >> n;
+  for (int i = 1; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  findCentroid(1, 0);
+  cout << centroid << endl;
+
+  return 0;
+}
